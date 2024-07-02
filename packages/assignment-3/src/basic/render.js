076c38a9388fc271a947fa36d39c7c40c5b27ff8
horang-e <rmsid1000@gmail.com>
2024-07-02 17:00:42 +0900

    fix: 코드 가독성 수정

--- packages/assignment-3/src/basic/render.js
+++ packages/assignment-3/src/basic/render.js
@@ -31,10 +31,10 @@ export function createElement(node) {
 
 function updateAttributes(target, newProps, oldProps) {
   // newProps들을 반복하여 각 속성과 값을 확인
-  //   만약 oldProps에 같은 속성이 있고 값이 동일하다면
-  //     다음 속성으로 넘어감 (변경 불필요)
-  //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
-  //     target에 해당 속성을 새 값으로 설정
+  // 만약 oldProps에 같은 속성이 있고 값이 동일하다면
+  // 다음 속성으로 넘어감 (변경 불필요)
+  // 만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
+  // target에 해당 속성을 새 값으로 설정
   newProps = newProps || {};
   oldProps = oldProps || {};
 
@@ -45,10 +45,10 @@ function updateAttributes(target, newProps, oldProps) {
   });
 
   // oldProps을 반복하여 각 속성 확인
-  //   만약 newProps들에 해당 속성이 존재한다면
-  //     다음 속성으로 넘어감 (속성 유지 필요)
-  //   만약 newProps들에 해당 속성이 존재하지 않는다면
-  //     target에서 해당 속성을 제거
+  // 만약 newProps들에 해당 속성이 존재한다면
+  // 다음 속성으로 넘어감 (속성 유지 필요)
+  // 만약 newProps들에 해당 속성이 존재하지 않는다면
+  // target에서 해당 속성을 제거
   Object.keys(oldProps).forEach(prop => {
     if (!(prop in newProps)) {
       target.removeAttribute(prop);
@@ -60,29 +60,29 @@ function updateAttributes(target, newProps, oldProps) {
 
 export function render(parent, newNode, oldNode, index = 0) {
   // 1. 만약 newNode가 없고 oldNode만 있다면
-  //   parent에서 oldNode를 제거
-  //   종료
+  // parent에서 oldNode를 제거
+  // 종료
   if (!newNode && oldNode) {
     return parent.removeChild(parent.childNodes[index]);
   }
 
   // 2. 만약 newNode가 있고 oldNode가 없다면
-  //   newNode를 생성하여 parent에 추가
-  //   종료
+  // newNode를 생성하여 parent에 추가
+  // 종료
   if (newNode && !oldNode) {
     return parent.appendChild(createElement(newNode));
   }
 
   // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
-  //   oldNode를 newNode로 교체
-  //   종료
+  // oldNode를 newNode로 교체
+  // 종료
   if (typeof newNode === 'string' && typeof oldNode === 'string' && newNode !== oldNode) {
     return parent.childNodes[index].nodeValue = newNode;
   }
 
   // 4. 만약 newNode와 oldNode의 타입이 다르다면
-  //   oldNode를 newNode로 교체
-  //   종료
+  // oldNode를 newNode로 교체
+  // 종료
   if (newNode.type !== oldNode.type) {
     return parent.replaceChild(createElement(newNode), parent.childNodes[index]);
   }
@@ -91,7 +91,7 @@ export function render(parent, newNode, oldNode, index = 0) {
   updateAttributes(parent.childNodes[index], newNode.props, oldNode.props);
 
   // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
-  //   각 자식노드에 대해 재귀적으로 render 함수 호출
+  // 각 자식노드에 대해 재귀적으로 render 함수 호출
   // 하위 노드 추가, 삭제에 대한 로직
   const newChildren = newNode.children || [];
   const oldChildren = oldNode.children || [];
