cabd22e71aec6fa9ed36f9cf845ac537cbba133d
horang-e <rmsid1000@gmail.com>
2024-07-16 18:03:19 +0900

    update: basic update

--- packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
+++ packages/assignment-5/src/refactoring/hooks/utils/cartUtils.ts
@@ -1,21 +1,50 @@
-import { CartItem, Coupon } from "../../../types";
+import { CartItem, Coupon } from '../../../types';
 
 export const calculateItemTotal = (item: CartItem) => {
-  return 0;
+  const discount = getMaxApplicableDiscount(item);
+  return item.product.price * item.quantity * (1 - discount);
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-  return 0;
+  // 할인율을 큰 숫자부터 정렬
+  const sortingDiscountList = item.product.discounts.sort((a, b) => b.rate - a.rate);
+
+  // 할인율 중 최대 할인율을 찾아서 적용
+  const applicableDiscount = sortingDiscountList.find((discount) => item.quantity >= discount.quantity);
+
+  return applicableDiscount?.rate || 0;
 };
 
 export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
+  const totalBeforeDiscount = cart.reduce((acc, item) => acc + item.product.price * item.quantity, 0);
+  const subtotalAfterProductDiscounts = cart.reduce((acc, item) => acc + calculateItemTotal(item), 0);
+
+  const applyCouponDiscount = (total: number, coupon: Coupon): number => {
+    const { discountType, discountValue } = coupon;
+    return discountType === 'amount' ? total - discountValue : total * (1 - discountValue / 100);
+  };
+
+  const totalAfterDiscount = selectedCoupon
+    ? applyCouponDiscount(subtotalAfterProductDiscounts, selectedCoupon)
+    : subtotalAfterProductDiscounts;
+
   return {
-    totalBeforeDiscount: 0,
-    totalAfterDiscount: 0,
-    totalDiscount: 0,
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount: totalBeforeDiscount - totalAfterDiscount,
   };
 };
 
 export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-  return []
+  const copiedCart = [...cart];
+  const itemIndex = copiedCart.findIndex((item) => item.product.id === productId);
+
+  if (itemIndex === -1) {
+    return copiedCart.filter((item) => item.quantity > 0);
+  }
+
+  const item = copiedCart[itemIndex];
+  item.quantity = Math.min(newQuantity, item.product.stock);
+
+  return copiedCart.filter((item) => item.quantity > 0);
 };
