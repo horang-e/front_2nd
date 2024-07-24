a33efb7a481b1ae3ce2a7f0c8b42cec9cbf437b5
horang-e <rmsid1000@gmail.com>
2024-07-24 19:08:30 +0900

    fix lighthouse again

--- packages/assignment-6/.lighthouse.js
+++ packages/assignment-6/.lighthouse.js
@@ -8,7 +8,9 @@ module.exports = {
       preset: "lighthouse:recommended",
     },
     upload: {
-      target: "temporary-public-storage",
+      target: "filesystem",
+      outputDir: "./lighthouse-results",  // 결과가 저장될 디렉토리
+      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%"
     },
   },
 };
